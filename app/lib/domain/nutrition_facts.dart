c82265ec1be75c6eef7e6e002687126d77625983
polydome <polydome@tutanota.com>
2022-05-30 11:09:36 +0200

    Remodel nutrition facts

--- app/lib/domain/nutrition_facts.dart
+++ app/lib/domain/nutrition_facts.dart
@@ -1,4 +1,17 @@
 class NutritionFacts {
-  final int calories = 0;
-  final int proteins = 0;
+  final int? energy;
+  final int? fat;
+  final int? carbohydrates;
+  final int? fibre;
+  final int? protein;
+  final int? salt;
+
+  const NutritionFacts({
+    this.energy,
+    this.fat,
+    this.carbohydrates,
+    this.fibre,
+    this.protein,
+    this.salt,
+  });
 }
