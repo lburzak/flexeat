7e9d380de52f3ff403cd99b630711009f7aaed94
polydome <polydome@tutanota.com>
2022-06-07 11:58:48 +0200

    Add supplied articles property to product

--- app/lib/domain/product.dart
+++ app/lib/domain/product.dart
@@ -1,30 +1,32 @@
 class Product {
   final int id;
   final String name;
+  final List<String> suppliedArticles;
 
-  const Product({
-    this.id = 0,
-    this.name = "",
-  });
+  const Product(
+      {this.id = 0, this.name = "", this.suppliedArticles = const []});
 
   @override
   bool operator ==(Object other) =>
       identical(this, other) ||
-      (other is Product &&
+      other is Product &&
           runtimeType == other.runtimeType &&
           id == other.id &&
-          name == other.name);
+          name == other.name &&
+          suppliedArticles == other.suppliedArticles;
 
   @override
-  int get hashCode => id.hashCode ^ name.hashCode;
+  int get hashCode => id.hashCode ^ name.hashCode ^ suppliedArticles.hashCode;
 
   Product copyWith({
     int? id,
     String? name,
+    List<String>? suppliedArticles,
   }) {
     return Product(
       id: id ?? this.id,
       name: name ?? this.name,
+      suppliedArticles: suppliedArticles ?? this.suppliedArticles,
     );
   }
 }
