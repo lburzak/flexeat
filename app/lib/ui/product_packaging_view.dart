86dbbb7004f7e9ad38cebc2023437bcd8d54ac23
polydome <polydome@tutanota.com>
2022-06-18 20:59:49 +0200

    Fix overflow

--- app/lib/ui/product_packaging_view.dart
+++ app/lib/ui/product_packaging_view.dart
@@ -14,23 +14,30 @@ class ProductPackagingView extends StatelessWidget {
       color: Theme.of(context).colorScheme.surfaceVariant,
       child: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Row(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(productPackaging.product.name),
-            const Spacer(),
-            Icon(
-              Icons.inventory,
-              color: Theme.of(context).colorScheme.onSurfaceVariant,
-              size: 16,
-            ),
             const SizedBox(
-              width: 8,
+              height: 8,
             ),
-            Text(productPackaging.packaging.label),
-            const SizedBox(
-              width: 8,
+            Row(
+              children: [
+                Icon(
+                  Icons.inventory,
+                  color: Theme.of(context).colorScheme.onSurfaceVariant,
+                  size: 16,
+                ),
+                const SizedBox(
+                  width: 8,
+                ),
+                Text(productPackaging.packaging.label),
+                const SizedBox(
+                  width: 8,
+                ),
+                Text("${productPackaging.packaging.weight} g")
+              ],
             ),
-            Text("${productPackaging.packaging.weight} g")
           ],
         ),
       ),
