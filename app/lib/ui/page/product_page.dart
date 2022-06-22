2fa1bff0acf3ba0c1db3b81f89f48ea1a3fb0a7f
polydome <polydome@tutanota.com>
2022-06-22 12:07:05 +0200

    Move navigation logic out of page section widget

--- app/lib/ui/page/product_page.dart
+++ app/lib/ui/page/product_page.dart
@@ -1,3 +1,5 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flexeat/bloc/navigation_cubit.dart';
 import 'package:flexeat/bloc/product_cubit.dart';
 import 'package:flexeat/bloc/product_packagings_cubit.dart';
 import 'package:flexeat/bloc/state/product_packagings_state.dart';
@@ -62,120 +64,132 @@ class _ProductPageState extends State<ProductPage> {
                 context.read<Factory<ProductCubit, int>>()(widget.productId))
       ],
       child: Builder(builder: (context) {
-        return Scaffold(
-          backgroundColor: Theme.of(context).colorScheme.surfaceVariant,
-          appBar: AppBar(
-            iconTheme: Theme.of(context).iconTheme.copyWith(
-                color: Theme.of(context).colorScheme.onSurfaceVariant),
-            actions: [
-              IconButton(
-                  onPressed: () {
-                    context.read<ProductCubit>().remove();
-                  },
-                  icon: const Icon(Icons.delete))
-            ],
-          ),
-          body: SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.only(
-                  left: 12, right: 12, top: 0, bottom: 12),
-              child: Column(
-                mainAxisSize: MainAxisSize.max,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 24.0),
-                    child: BlocBuilder<ProductCubit, ProductState>(
-                      builder: (context, state) {
-                        return EditableHeader(
-                          initialText: state.productName,
-                          style: Theme.of(context)
-                              .textTheme
-                              .headline1
-                              ?.copyWith(
-                                  color: Theme.of(context)
-                                      .colorScheme
-                                      .onSurfaceVariant),
-                          onSubmit: (text) =>
-                              context.read<ProductCubit>().changeName(text),
-                        );
+        return BlocListener<NavigationCubit, NavigationState>(
+            listener: (context, state) {
+              if (state is BackNavigationState) {
+                context.router.pop();
+              }
+            },
+            child: Scaffold(
+              backgroundColor: Theme.of(context).colorScheme.surfaceVariant,
+              appBar: AppBar(
+                iconTheme: Theme.of(context).iconTheme.copyWith(
+                    color: Theme.of(context).colorScheme.onSurfaceVariant),
+                actions: [
+                  IconButton(
+                      onPressed: () {
+                        context.read<ProductCubit>().remove();
                       },
-                    ),
-                  ),
-                  PageSection(
-                      icon: Icons.local_dining,
-                      label: "Nutrition Facts",
-                      body: BlocBuilder<ProductCubit, ProductState>(
-                        builder: (context, state) {
-                          final map = state.nutritionFacts.toMap();
-                          if (map.values.every((element) => element == null)) {
-                            return SizedBox(
-                                width: double.infinity,
-                                child: ElevatedButton(
-                                    onPressed: () =>
-                                        _showNutritionFactsDialog(context),
-                                    child: Text(
-                                      "Add".toUpperCase(),
-                                    )));
-                          }
-                          return NutritionFactsView(
-                              color: Theme.of(context)
-                                  .colorScheme
-                                  .onSurfaceVariant,
-                              nutritionFacts: state.nutritionFacts,
-                              onEdit: () => _showNutritionFactsDialog(context));
-                        },
-                      )),
-                  PageSection(
-                      icon: Icons.inventory,
-                      label: "Packagings",
-                      body: BlocBuilder<ProductPackagingsCubit,
-                              ProductPackagingsState>(
-                          builder: (context, state) => PackagingSelector(
-                                packagings: state.packagings,
-                                onAdd: () {
-                                  _showPackagingDialog(context);
-                                },
-                              ))),
-                  PageSection(
-                      icon: Icons.link,
-                      label: "Used as",
-                      body: BlocBuilder<ProductCubit, ProductState>(
-                          builder: (context, state) => ArticlesListView(
-                                articles: state.compatibleArticles,
-                                onUnlink: (articleId) => context
-                                    .read<ProductCubit>()
-                                    .unlinkArticle(articleId),
-                                onAdd: () => showModalBottomSheet(
-                                    context: context,
-                                    builder: (context) => Provider(
-                                          create: (context) => context.read<
-                                                  Factory<ProductCubit, int>>()(
-                                              widget.productId),
-                                          child: Builder(builder: (context) {
-                                            return LinkArticleView(
-                                                onSubmitArticle: (article) {
-                                                  context
-                                                      .read<ProductCubit>()
-                                                      .linkArticle(article.id);
-                                                  Navigator.of(context).pop();
-                                                },
-                                                onSubmitText: (text) {
-                                                  context
-                                                      .read<ProductCubit>()
-                                                      .linkNewArticle(text);
-                                                  Navigator.of(context).pop();
-                                                },
-                                                articles:
-                                                    state.availableArticles);
-                                          }),
-                                        )),
-                              ))),
+                      icon: const Icon(Icons.delete))
                 ],
               ),
-            ),
-          ),
-        );
+              body: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                      left: 12, right: 12, top: 0, bottom: 12),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.max,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 24.0),
+                        child: BlocBuilder<ProductCubit, ProductState>(
+                          builder: (context, state) {
+                            return EditableHeader(
+                              initialText: state.productName,
+                              style: Theme.of(context)
+                                  .textTheme
+                                  .headline1
+                                  ?.copyWith(
+                                      color: Theme.of(context)
+                                          .colorScheme
+                                          .onSurfaceVariant),
+                              onSubmit: (text) =>
+                                  context.read<ProductCubit>().changeName(text),
+                            );
+                          },
+                        ),
+                      ),
+                      PageSection(
+                          icon: Icons.local_dining,
+                          label: "Nutrition Facts",
+                          body: BlocBuilder<ProductCubit, ProductState>(
+                            builder: (context, state) {
+                              final map = state.nutritionFacts.toMap();
+                              if (map.values
+                                  .every((element) => element == null)) {
+                                return SizedBox(
+                                    width: double.infinity,
+                                    child: ElevatedButton(
+                                        onPressed: () =>
+                                            _showNutritionFactsDialog(context),
+                                        child: Text(
+                                          "Add".toUpperCase(),
+                                        )));
+                              }
+                              return NutritionFactsView(
+                                  color: Theme.of(context)
+                                      .colorScheme
+                                      .onSurfaceVariant,
+                                  nutritionFacts: state.nutritionFacts,
+                                  onEdit: () =>
+                                      _showNutritionFactsDialog(context));
+                            },
+                          )),
+                      PageSection(
+                          icon: Icons.inventory,
+                          label: "Packagings",
+                          body: BlocBuilder<ProductPackagingsCubit,
+                                  ProductPackagingsState>(
+                              builder: (context, state) => PackagingSelector(
+                                    packagings: state.packagings,
+                                    onAdd: () {
+                                      _showPackagingDialog(context);
+                                    },
+                                  ))),
+                      PageSection(
+                          icon: Icons.link,
+                          label: "Used as",
+                          body: BlocBuilder<ProductCubit, ProductState>(
+                              builder: (context, state) => ArticlesListView(
+                                    articles: state.compatibleArticles,
+                                    onUnlink: (articleId) => context
+                                        .read<ProductCubit>()
+                                        .unlinkArticle(articleId),
+                                    onAdd: () => showModalBottomSheet(
+                                        context: context,
+                                        builder: (context) => Provider(
+                                              create: (context) => context.read<
+                                                  Factory<ProductCubit,
+                                                      int>>()(widget.productId),
+                                              child:
+                                                  Builder(builder: (context) {
+                                                return LinkArticleView(
+                                                    onSubmitArticle: (article) {
+                                                      context
+                                                          .read<ProductCubit>()
+                                                          .linkArticle(
+                                                              article.id);
+                                                      Navigator.of(context)
+                                                          .pop();
+                                                    },
+                                                    onSubmitText: (text) {
+                                                      context
+                                                          .read<ProductCubit>()
+                                                          .linkNewArticle(text);
+                                                      Navigator.of(context)
+                                                          .pop();
+                                                    },
+                                                    articles: state
+                                                        .availableArticles);
+                                              }),
+                                            )),
+                                  ))),
+                    ],
+                  ),
+                ),
+              ),
+            ));
       }),
     );
   }
--- app/lib/ui/widget/page_section.dart
+++ app/lib/ui/widget/page_section.dart
@@ -1,7 +1,4 @@
-import 'package:auto_route/auto_route.dart';
-import 'package:flexeat/bloc/navigation_cubit.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 class PageSection extends StatelessWidget {
   final IconData icon;
@@ -14,35 +11,28 @@ class PageSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocListener<NavigationCubit, NavigationState>(
-      listener: (context, state) {
-        if (state is BackNavigationState) {
-          context.router.pop();
-        }
-      },
-      child: Column(
-        mainAxisSize: MainAxisSize.max,
-        children: [
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 12),
-            child: Column(
-              children: [
-                Row(
-                  children: [
-                    Icon(icon,
-                        color: Theme.of(context).colorScheme.onSurfaceVariant),
-                    const SizedBox(
-                      width: 12,
-                    ),
-                    Text(label)
-                  ],
-                ),
-              ],
-            ),
+    return Column(
+      mainAxisSize: MainAxisSize.max,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 12),
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  Icon(icon,
+                      color: Theme.of(context).colorScheme.onSurfaceVariant),
+                  const SizedBox(
+                    width: 12,
+                  ),
+                  Text(label)
+                ],
+              ),
+            ],
           ),
-          body,
-        ],
-      ),
+        ),
+        body,
+      ],
     );
   }
 }
